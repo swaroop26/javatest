5450f0ddcfd336cce89bdd0f15d73f2bd24ae38e
Kranthi Syamala <kranthi.syamala@sportsbet.com.au>
2022-02-04 14:42:15 +1100

    fixing message

--- src/main/java/com/altx/javatest/exception/ActorNotFoundException.java
+++ src/main/java/com/altx/javatest/exception/ActorNotFoundException.java
@@ -3,7 +3,7 @@ package com.altx.javatest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Actor Not Found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ActorNotFoundException extends RuntimeException {
     public ActorNotFoundException(Long id) {
         super("Could not find the actor " + id);
--- src/main/java/com/altx/javatest/exception/MovieNotFoundException.java
+++ src/main/java/com/altx/javatest/exception/MovieNotFoundException.java
@@ -3,7 +3,7 @@ package com.altx.javatest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Movie Not Found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class MovieNotFoundException extends RuntimeException {
     public MovieNotFoundException(Long id) {
         super("Could not find movie " + id);
